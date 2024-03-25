a195dc03f4089c593cad1ad3068534f9742f6175
yoz75 <levtannn@gmail.com>
2024-03-25 15:46:26 +0300

    Rule now uses limited number of iterations

--- Rule.cs
+++ Rule.cs
@@ -6,6 +6,15 @@ namespace Celluros
 {
     public class Rule
     {
+
+        private int CurrentIteration = 0;
+        private int MaxIteration;
+
+        public Rule(int iterations)
+        {
+            MaxIteration = iterations;
+        }
+
         public List<Condition> Conditions
         {
             get;
@@ -14,6 +23,13 @@ namespace Celluros
 
         public void Execute(Field field)
         {
+
+            if(CurrentIteration >= MaxIteration)
+            {
+                return;
+            }
+
+            CurrentIteration ++;
             foreach (var condition in Conditions) 
             {
                 condition.Calculate(field);
