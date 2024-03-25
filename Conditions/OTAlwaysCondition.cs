8efec563122692a9c6fa506c1f8256f294943ed5
yoz75 <levtannn@gmail.com>
2024-03-25 17:19:48 +0300

    Added One Type Always Condition

---
+++ Conditions/OTAlwaysCondition.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+namespace Celluros.Conditions
+{
+    public class OTAlwaysCondition : Condition
+    {
+
+        private Cell StartType;
+        private Cell EndType;
+        private float Chance;
+
+        public OTAlwaysCondition(float chance, Cell startType, Cell endType)
+        {
+            StartType = startType;
+            Chance = chance;
+            EndType = endType;
+        }
+
+        public override void Calculate(in Field field)
+        {
+            Random random = new Random();
+
+            Cell[,] newFrame = field.GetField();
+
+            for(int x = 0; x < field.GetLength(0); x++)
+            {
+                for(int y = 0; y < field.GetLength(1); y++)
+                {
+                    if(field.IsTypeAtPosition(x,y, StartType))
+                    {
+                        if(random.Next(0, 100) < Chance)
+                        {
+
+
+                        }
+                        newFrame.SetAtPosition(x, y, EndType);
+                    }
+                }
+            }
+
+            field.SetField(newFrame);
+
+        }
+    }
+}
